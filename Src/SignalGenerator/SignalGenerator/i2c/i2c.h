ef4c175c3a37469f1a2e37d530ed267cd286fafe
Joachim Westlund <jocke@jocke.net>
2022-03-08 20:14:25 +0100

    Adding some software things

---
+++ Src/SignalGenerator/SignalGenerator/i2c/i2c.h
@@ -0,0 +1,16 @@
+/*
+ * i2c.h
+ *
+ * Created: 2022-02-27 14:24:28
+ *  Author: Joachim Westlund
+ */ 
+
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+void 
+
+
+#endif /* I2C_H_ */
---
+++ Src/SignalGenerator/SignalGenerator/main.c
@@ -0,0 +1,23 @@
+/** \File */
+
+/**
+ * SignalGenerator.c
+ *
+ * Created: 2022-02-27 13:12:30
+ * Author : Joachim Westlund
+ */ 
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+
+int main(void)
+{
+	lcd2w_init();
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
